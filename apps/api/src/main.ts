ff0ce44534c2998e59063c71de28f4b5c3aa0d3b
Matheus Wallace <matheuswts2011@hotmail.com>
2022-11-13 23:00:51 -0300

    add database configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,15 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import 'reflect-metadata';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
