5b65815d6cee5ce30c68b9249fe9f5c15ef5b21b
Matheus Wallace <matheuswts2011@hotmail.com>
2022-11-20 13:56:24 -0300

    rename app

--- apps/admin/jest.config.ts
+++ apps/admin/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'sga',
+  displayName: 'admin',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/sga',
+  coverageDirectory: '../../coverage/apps/admin',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
